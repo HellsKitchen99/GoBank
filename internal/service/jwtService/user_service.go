65c83d95dcef8e56cd5c308e7da05b058fd5dd1c
Punisher <palekseev102@gmail.com>
2025-10-13 20:13:09 +0300

    fix(service): change JwtService to UserService in user_service.go

--- internal/service/jwtService/user_service.go
+++ internal/service/jwtService/user_service.go
@@ -4,24 +4,36 @@ import (
 	"GoBank/internal/domain"
 	"GoBank/internal/repository"
 	"context"
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
 )
 
-type JwtService struct {
+type UserService struct {
 	repo repository.UserRepository
 }
 
-func NewJwtService(repo repository.UserRepository) *JwtService {
-	var jwtService JwtService = JwtService{
+func NewJwtService(repo repository.UserRepository) *UserService {
+	var userService UserService = UserService{
 		repo: repo,
 	}
-	return &jwtService
+	return &userService
 }
 
-func (j *JwtService) Login(ctx context.Context, email string, password string) {
-
+func (j *UserService) Login(ctx context.Context, email string, password string) error {
+	_, err := j.GetUserDetails(ctx, email)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return fmt.Errorf("user not found")
+		}
+		return err
+	}
+	//uенерация токена
+	return nil
 }
 
-func (j *JwtService) GetUserDetails(ctx context.Context, email string) (domain.User, error) {
+func (j *UserService) GetUserDetails(ctx context.Context, email string) (domain.User, error) {
 	user, err := j.repo.CheckUserInDataBase(ctx, email)
 	if err != nil {
 		return user, err
