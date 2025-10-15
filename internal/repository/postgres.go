9ee6ab3268ca53fc47f4888201bcf2f6a49177a0
Punisher <palekseev102@gmail.com>
2025-10-15 11:26:04 +0300

    feat(repository): add CreateUser func relization

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -26,10 +26,26 @@ func (r *UserRepo) CheckUserInDataBase(ctx context.Context, email string) (domai
 	query := `SELECT id, name, email, password, roles FROM users WHERE email=$1`
 	row := r.db.QueryRow(ctx, query, email)
 	if err := row.Scan(&user.Id, &user.Name, &user.Email, &user.Password, &user.Roles); err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return user, fmt.Errorf("user not found")
-		}
 		return user, err
 	}
 	return user, nil
+}
+
+func (r *UserRepo) CreateUser(ctx context.Context, name, email, password string, roles []string) error {
+	_, err := r.CheckUserInDataBase(ctx, email)
+	if err == nil {
+		return fmt.Errorf("user already exists")
+	}
+	if !errors.Is(err, pgx.ErrNoRows) {
+		return err
+	}
+	query := `INSERT INTO users (name, email, password, roles) VALUES ($1, $2, $3, $4)`
+	tag, err := r.db.Exec(ctx, query, name, email, password, roles)
+	if err != nil {
+		return err
+	}
+	if tag.RowsAffected() != 1 {
+		return fmt.Errorf("bad insert")
+	}
+	return nil
 }
