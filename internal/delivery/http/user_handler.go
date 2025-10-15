3c0c15935ee2d5a1265b01441befcf645edcbb1e
Punisher <palekseev102@gmail.com>
2025-10-15 13:52:59 +0300

    feat(http): add Register handler realization

--- internal/delivery/http/user_handler.go
+++ internal/delivery/http/user_handler.go
@@ -1,7 +1,11 @@
 package http
 
 import (
+	"GoBank/internal/domain"
 	"GoBank/internal/usecase"
+	"context"
+	"errors"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,8 +26,34 @@ func (h *UserHandler) RegisterRoutes(r *gin.Engine) {
 	r.POST("/login", h.Login)
 }
 
-func (h *UserHandler) Register(ctx *gin.Context) {
-	//заглушка
+func (h *UserHandler) Register(c *gin.Context) {
+	var userFromFront domain.UserFromFront
+
+	if err := c.ShouldBindJSON(&userFromFront); err != nil {
+		c.JSON(400, gin.H{
+			"error": "bad json",
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	err := h.service.Register(ctx, userFromFront.Name, userFromFront.Email, userFromFront.Password)
+
+	if errors.Is(err, usecase.ErrUserAlreadyExists) {
+		c.JSON(409, gin.H{
+			"error": err.Error(),
+		})
+		return
+	} else if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(201, gin.H{
+		"answer": "user has been created succesfully",
+	})
 }
 
 func (h *UserHandler) Login(ctx *gin.Context) {
