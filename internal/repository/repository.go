e9f7cbfa677551d0d702463195ebe3183a159996
Punisher <palekseev102@gmail.com>
2025-10-16 19:14:24 +0300

    feat(repository): add interface with transactions and database work

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,9 +3,19 @@ package repository
 import (
 	"GoBank/internal/domain"
 	"context"
+	"time"
 )
 
 type UserRepository interface {
 	CheckUserInDataBase(ctx context.Context, email string) (domain.User, error)
 	CreateUser(ctx context.Context, name, email, password string, roles []string) (int64, error)
+	CheckUserInDataBaseById(ctx context.Context, id int64) bool
+}
+
+type TransactionRepository interface {
+	CreateTransaction(ctx context.Context, from, to int64, amount float64, timeOfCreation time.Time, status string) error
+	GetAmountOfSenderFromDataBase(ctx context.Context, from int64) (float64, error)
+	CheckUserToSendInDataBase() error
+	MinusMoney(amount float64) error
+	AddMoney(amount float64) error
 }
