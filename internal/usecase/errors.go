58facb07307174907e7ed77f9d81a52a3d88cb0a
Punisher <palekseev102@gmail.com>
2025-10-16 19:13:13 +0300

    feat(service): add 2 erorrs for validation transaction

--- internal/usecase/errors.go
+++ internal/usecase/errors.go
@@ -5,3 +5,5 @@ import "errors"
 var ErrUserAlreadyExists = errors.New("user already exists")
 var ErrUserNotFound = errors.New("user not found")
 var ErrWrongPassword = errors.New("wrong password")
+var ErrAmountToTransfer = errors.New("not enough balance")
+var ErrSameUser = errors.New("you cant't transfer yourself")
