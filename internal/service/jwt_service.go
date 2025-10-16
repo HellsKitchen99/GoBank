c634f9ae6182ec3f8edfcf800caa086bf6e33fd6
Punisher <palekseev102@gmail.com>
2025-10-16 12:08:03 +0300

    feat(service): add token validation logic

--- internal/service/jwt_service.go
+++ internal/service/jwt_service.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"GoBank/internal/repository"
+	"context"
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -9,12 +12,14 @@ import (
 type JwtService struct {
 	Key            []byte
 	ExpirationTime time.Duration
+	repo           *repository.UserRepo
 }
 
-func NewJwtService(key string, exp time.Duration) *JwtService {
+func NewJwtService(key string, exp time.Duration, repo *repository.UserRepo) *JwtService {
 	var jwtService JwtService = JwtService{
 		Key:            []byte(key),
 		ExpirationTime: exp,
+		repo:           repo,
 	}
 	return &jwtService
 }
@@ -37,4 +42,54 @@ func (j *JwtService) GenerateJwtToken(id int64, roles []string) (string, error) 
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(j.Key)
+}
+
+func (j *JwtService) TokenValidation(token string) bool {
+	var jwtKey []byte = j.Key
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(jwtKey), nil
+	})
+	if err != nil {
+		return false
+	}
+
+	if !parsedToken.Valid {
+		return false
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return false
+	}
+
+	exp, ok := claims["exp"].(float64)
+	if !ok {
+		return false
+	}
+	if time.Now().Unix() > int64(exp) {
+		return false
+	}
+	signer, ok := claims["iss"].(string)
+	if !ok {
+		return false
+	}
+	if signer != "GoBank" {
+		return false
+	}
+
+	userId, ok := claims["user_id"].(float64)
+	if !ok {
+		return false
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	if find := j.repo.CheckUserInDataBaseById(ctx, int64(userId)); !find {
+		return false
+	}
+	return true
 }
